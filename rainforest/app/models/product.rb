259f4c80a2a6e43cdf4eb980c18e8fc10abc7c64
alanjkg <alanjkg@gmail.com>
2014-02-03 18:47:56 -0500

    Writing product validations

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+	validates :description, :name, :url, :presence => true
+	validates :price_in_cents, :numericality => {:only_integer => true}
+
+	validates :url, presence: true
+
+	validates_presence_of :name, :description, :url
 end
