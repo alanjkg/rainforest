d2d73d1466351a8ace6072c3929545e7fbca5e73
alanjkg <alanjkg@gmail.com>
2014-02-03 23:17:26 -0500

    displaying validation errors on the product form

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 	validates :description, :name, :url, :presence => true
 	validates :price_in_cents, :numericality => {:only_integer => true}
 
-	validates :url, presence: true
+	#validates :url, presence: true
 
-	validates_presence_of :name, :description, :url
+	#validates_presence_of :name, :description, :url
 end
