2b79d03088a6ecabb312ebf8034044dc0fde5bd9
alanjkg <alanjkg@gmail.com>
2014-02-04 12:10:26 -0500

    Displaying price in dollars

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -5,4 +5,9 @@ class Product < ActiveRecord::Base
 	#validates :url, presence: true
 
 	#validates_presence_of :name, :description, :url
+
+	def formatted_price
+        price_in_dollars = price_in_cents.to_f / 100
+        sprintf("%.2f", price_in_dollars)
+    end
 end
